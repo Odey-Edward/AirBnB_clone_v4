17c6a33718c69e3f7d483f3c9d0b2257d851a10a
Edward-Odey <edwardodey04@gmail.com>
2024-01-15 15:26:58 +0100

    added api status check

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+	$.get('http://localhost:5001/api/v1/status/', function (data) {
+		if (data.status === 'OK') {
+			$('div#api_status').addClass('available');
+		} else {
+			$('div#api_status').removeClass('available');
+		}
+	});
+
+	let selectedAmenities = {};
+
+	$('input[type="checkbox"]').change(function () {
+		let amenityId = $(this).data('id');
+		let amenityName = $(this).data('name');
+
+		if ($(this).prop('checked')) {
+			selectedAmenities[amenityId] = amenityName;
+		} else {
+			delete selectedAmenities[amenityId];
+		}
+
+		let amenitiesList = Object.values(selectedAmenities).join(', ');
+		$('.amenities h4').text(amenitiesList);
+	});
+});
